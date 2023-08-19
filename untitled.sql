e3bb1d8244cd03adb21f144077238ed936b981c4
Alexander Korotaev <73218177+alexkandinsky@users.noreply.github.com>
2023-08-19 14:46:52 +0300

    Update untitled.sql

--- untitled.sql
+++ untitled.sql
@@ -10,3 +10,12 @@ LEFT JOIN users as u
 USING(user_id)
 WHERE  u.user_id is not null
 ORDER BY 1
+
+
+SELECT f.status, f.member_name, SUM(p.unit_price * p.amount) as costs
+FROM FamilyMembers as f
+INNER JOIN Payments as p ON f.member_id = p.family_member
+INNER JOIN Goods as g ON p.good = g.good_id
+INNER JOIN GoodTypes as gt ON g.type = gt.good_type_id
+WHERE gt.good_type_name = 'entertainment'
+GROUP BY f.status, f.member_name
