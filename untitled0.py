96602328c93e0f6c04eef9d8e673e8554ea08e7e
Alexander Korotaev <73218177+alexkandinsky@users.noreply.github.com>
2023-08-19 14:42:19 +0300

    Delete untitled0.py

--- untitled0.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-def is_leap(year):
-    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
-# Example
-year = 2020
-if is_leap(year):
-    print(f"The year {year} is a leap year")
-else:
-    print(f"The year {year} is not a leap year")
